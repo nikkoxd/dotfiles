987c7aa17bd6b676b08bbd7f1e0aa574b62a7e33
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-08 20:43:07 +0400

    feat: better space switcher

---
+++ sketchybar/items/spaces.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+for i in "${!SPACE_ICONS[@]}"
+do
+  sid="$(($i+1))"
+  space=(
+    space="$sid"
+    icon="${SPACE_ICONS[i]}"
+    padding_left=0
+    padding_right=0
+    icon.padding_left=10
+    icon.padding_right=10
+    icon.highlight_color=$BLACK
+    background.color=$ACCENT
+    background.corner_radius=5
+    background.height=25
+    label.drawing=off
+    script="$PLUGIN_DIR/space.sh"
+  )
+  sketchybar --add space space.$sid left \
+             --set space.$sid "${space[@]}" \
+             --subscribe space.$sid mouse.clicked
+done
+
+spaces=(
+  background.color=$BACKGROUND2
+  background.border_color=$ACCENT
+  background.border_width=2
+  background.corner_radius=5
+  background.height=25
+  background.drawing=on
+)
+
+sketchybar --add bracket spaces '/space\..*/' \
+           --set spaces "${spaces[@]}"
+
+chevron=(
+  icon= 
+  padding_left=10
+  icon.font="$FONT:Bold:14.0"
+  icon.padding_left=7
+  icon.padding_right=5
+  icon.color=$BLACK
+  label.drawing=off
+  background.color=$ACCENT
+  background.height=25
+  background.corner_radius=5
+  click_script="yabai -m space --create && sketchybar --trigger space_change"
+)
+
+sketchybar --add item chevron left \
+           --set chevron "${chevron[@]}"
+
+sketchybar --add item front_app left \
+           --set front_app icon.drawing=off script="$PLUGIN_DIR/front_app.sh" \
+           --subscribe front_app front_app_switched
--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
+update() {
+  sketchybar --set "$NAME" icon.highlight="$SELECTED" background.drawing="$SELECTED"
+}
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+mouse_clicked() {
+  if [ "$BUTTON" = "right" ]; then
+    yabai -m space --destroy $SID
+    sketchybar --trigger space_change --trigger windows_on_spaces
+  else
+    yabai -m space --focus $SID 2>/dev/null
+  fi
+}
+
+case "$SENDER" in
+  "mouse.clicked") mouse_clicked
+  ;;
+  *) update
+  ;;
+esac
