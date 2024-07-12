9a5b9c9eb6bec20c116993be1b008d88268ff538
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-12 17:44:32 +0400

    feat: add neorg

---
+++ nvim/lua/plugins/neorg.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neorg/neorg",
+  version = "*",
+  dependencies = {
+    {
+      "vhyrro/luarocks.nvim",
+      priority=1000,
+      config = true
+    }
+  },
+  config = true
+}
