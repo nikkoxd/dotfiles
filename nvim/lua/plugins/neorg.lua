1dd2dfb7502b06b26fb168700f5403b346a33199
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-13 00:36:47 +0400

    fix: add image support

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -5,13 +5,26 @@ return {
     {
       "vhyrro/luarocks.nvim",
       priority=1000,
+      opts = {
+        rocks = { "magick" },
+      }
+    },
+    {
+      "3rd/image.nvim",
       opts = {}
-    }
+    },
   },
   opts = {
     load = {
       ["core.defaults"] = {},
+      ["core.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+        },
+      },
+      ["core.integrations.nvim-cmp"] = {},
       ["core.concealer"] = {},
+      ["core.integrations.image"] = {},
     }
   }
 }
