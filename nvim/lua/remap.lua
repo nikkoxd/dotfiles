5c734c4cd527fc63ec9f576d2630c532a9ccb34a
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-12 23:35:35 +0400

    fix: add remap for making splits

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -3,4 +3,5 @@ vim.g.maplocalleader = ","
 
 vim.keymap.set("n", "<leader>yf", "<cmd>%y+<CR>")
 
-vim.keymap.set("n", "<leader>w", "<C-w>")
+vim.keymap.set("n", "<C-w>-", "<cmd>split<CR>")
+vim.keymap.set("n", "<C-w>|", "<cmd>vsplit<CR>")
