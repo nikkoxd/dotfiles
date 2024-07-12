ab11701e504fb854e2679b4e43f0611020941f59
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-12 17:44:14 +0400

    fix: set treesitter compiler to gcc-14

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,21 +7,26 @@ return {
     { "<leader>tr", function() require("telescope.builtin").treesitter() end, desc = "Treesitter" },
   },
 
-  opts = {
-    ensure_installed = {
-      "gdscript",
-      "godot_resource",
-      "gdshader",
-      "lua",
-      "vim",
-      "vimdoc",
-      "query",
-      "typescript",
-      "astro",
-      "python",
-    },
-    sync_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  config = function()
+    require("nvim-treesitter.install").compilers = { "gcc-14" }
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = {
+        "gdscript",
+        "godot_resource",
+        "gdshader",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "typescript",
+        "astro",
+        "python",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
