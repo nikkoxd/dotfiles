f0335d5deb6c391710e55bde23025403e4d77241
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-12 23:35:13 +0400

    chore: remove commented out code

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,14 +4,4 @@ return {
   config = function()
     vim.cmd("colorscheme pywal16")
   end
-  -- "rose-pine/neovim",
-  -- name = "rose-pine",
-  -- config = function()
-  --   require("rose-pine").setup({
-  --     styles = {
-  --       transparency = true,
-  --     }
-  --   })
-  --   vim.cmd("colorscheme rose-pine")
-  -- end
 }
