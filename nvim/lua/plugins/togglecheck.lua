ab6ef70eb8a8ef1bf03d2b0131e9e204988fa3ba
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-12 16:22:50 +0400

    fix: remove togglecheck

--- nvim/lua/plugins/togglecheck.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nfrid/markdown-togglecheck",
-  dependencies = { "nfrid/treesitter-utils" },
-  ft = { "markdown" },
-  keys = {
-    { "<leader>t", function() require("markdown-togglecheck").toggle() end, { desc = "Toggle checkbox" } },
-  }
-}
