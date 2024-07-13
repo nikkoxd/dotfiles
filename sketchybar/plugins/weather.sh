20b040ef325bfb7c36c39c63ea14484e5cb6b43a
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-13 18:08:01 +0400

    fix: disable drawing if service is not available

--- sketchybar/plugins/weather.sh
+++ sketchybar/plugins/weather.sh
@@ -3,104 +3,53 @@
 WEATHER=$(curl -s https://wttr.in/Izhevsk?format=%C)
 TEMPERATURE=$(curl -s https://wttr.in/Izhevsk?format=%t)
 
-echo "'$WEATHER'"
-
-if [ "$WEATHER" == "Clear " ]; then
-    ICON="􀆬"
-elif [ "$WEATHER" == "Sunny" ]; then
-    ICON="􀆮"
-elif [ "$WEATHER" == "Partly cloudy" ]; then
-    ICON="􀇕"
-elif [ "$WEATHER" == "Cloudy" ]; then
-    ICON="􀇃"
-elif [ "$WEATHER" == "Overcast" ]; then
-    ICON="􀇃"
-elif [ "$WEATHER" == "Mist" ]; then
-    ICON="􀇋"
-elif [ "$WEATHER" == "Patchy rain possible" ]; then
-    ICON="􀇅"
-elif [ "$WEATHER" == "Patchy snow possible" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Patchy sleet possible" ]; then
-    ICON="􀇑"
-elif [ "$WEATHER" == "Patchy freezing drizzle possible" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Thundery outbreaks possible" ]; then
-    ICON="􀇟"
-elif [ "$WEATHER" == "Blowing snow" ]; then
-    ICON="􀇦"
-elif [ "$WEATHER" == "Blizzard" ]; then
-    ICON="􀇦"
-elif [ "$WEATHER" == "Fog" ]; then
-    ICON="􀇋"
-elif [ "$WEATHER" == "Freezing fog" ]; then
-    ICON="􀇋"
-elif [ "$WEATHER" == "Patchy light drizzle" ]; then
-    ICON="􀇅"
-elif [ "$WEATHER" == "Light drizzle" ]; then
-    ICON="􀇅"
-elif [ "$WEATHER" == "Freezing drizzle" ]; then
-    ICON="􀇅"
-elif [ "$WEATHER" == "Heavy freezing drizzle" ]; then
-    ICON="􀇅"
-elif [ "$WEATHER" == "Patchy light rain" ]; then
-    ICON="􀇅"
-elif [ "$WEATHER" == "Light rain" ]; then
-    ICON="􀇅"
-elif [ "$WEATHER" == "Moderate rain at times" ]; then
-    ICON="􀇉"
-elif [ "$WEATHER" == "Moderate rain" ]; then
-    ICON="􀇉"
-elif [ "$WEATHER" == "Heavy rain at times" ]; then
-    ICON="􀇉"
-elif [ "$WEATHER" == "Heavy rain" ]; then
-    ICON="􀇉"
-elif [ "$WEATHER" == "Light freezing rain" ]; then
-    ICON="􀇅"
-elif [ "$WEATHER" == "Moderate or heavy freezing rain" ]; then
-    ICON="􀇇"
-elif [ "$WEATHER" == "Light sleet" ]; then
-    ICON="􀇑"
-elif [ "$WEATHER" == "Moderate or heavy sleet" ]; then
-    ICON="􀇑"
-elif [ "$WEATHER" == "Patchy light snow" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Light snow" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Patchy moderate snow" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Moderate snow" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Patchy heavy snow" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Heavy snow" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Ice pellets" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Light rain shower" ]; then
-    ICON="􀇅"
-elif [ "$WEATHER" == "Moderate or heavy rain showers" ]; then
-    ICON="􀇇"
-elif [ "$WEATHER" == "Torrential rain showers" ]; then
-    ICON="􀇉"
-elif [ "$WEATHER" == "Light sleet showers" ]; then
-    ICON="􀇑"
-elif [ "$WEATHER" == "Moderate or heavy sleet showers" ]; then
-    ICON="􀇑"
-elif [ "$WEATHER" == "Light snow shower" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Moderate or heavy snow showers" ]; then
-    ICON="􀇏"
-elif [ "$WEATHER" == "Patchy light rain with thunder" ]; then
-    ICON="􀇟"
-elif [ "$WEATHER" == "Moderate or heavy rain with thunder" ]; then
-    ICON="􀇟"
-elif [ "$WEATHER" == "Patchy light snow with thunder" ]; then
-    ICON="􀇓"
-elif [ "$WEATHER" == "Moderate or heavy snow with thunder" ]; then
-    ICON="􀇓"
+if [[ "$WEATHER" != *"Unknown"* ]]; then
+  case "$WEATHER" in
+    "Clear ")
+      ICON="􀆬"
+      ;;
+    "Sunny")
+      ICON="􀆮"
+      ;;
+    "Partly cloudy")
+      ICON="􀇕"
+      ;;
+    "Cloudy" | "Overcast")
+      ICON="􀇃"
+      ;;
+    "Mist" | "Fog" | "Freezing fog")
+      ICON="􀇋"
+      ;;
+    "Patchy rain possible" | "Patchy light drizzle" | "Light drizzle" | "Freezing drizzle" | "Heavy freezing drizzle" | "Patchy light rain" | "Light rain" | "Light freezing rain" | "Light rain shower")
+      ICON="􀇅"
+      ;;
+    "Patchy snow possible" | "Patchy freezing drizzle possible" | "Patchy light snow" | "Light snow" | "Patchy moderate snow" | "Moderate snow" | "Patchy heavy snow" | "Heavy snow" | "Ice pellets" | "Light snow shower" | "Moderate or heavy snow showers")
+      ICON="􀇏"
+      ;;
+    "Patchy sleet possible" | "Light sleet" | "Moderate or heavy sleet" | "Light sleet showers" | "Moderate or heavy sleet showers")
+      ICON="􀇑"
+      ;;
+    "Thundery outbreaks possible" | "Patchy light rain with thunder" | "Moderate or heavy rain with thunder")
+      ICON="􀇟"
+      ;;
+    "Blowing snow" | "Blizzard")
+      ICON="􀇦"
+      ;;
+    "Moderate rain at times" | "Moderate rain" | "Heavy rain at times" | "Heavy rain" | "Moderate or heavy rain showers" | "Moderate or heavy freezing rain")
+      ICON="􀇉"
+      ;;
+    "Patchy light snow with thunder" | "Moderate or heavy snow with thunder")
+      ICON="􀇓"
+      ;;
+    "Torrential rain showers")
+      ICON="􀇉"
+      ;;
+    *)
+      ICON=""
+      ;;
+  esac
+  sketchybar --set "$NAME" icon="$ICON" label="$TEMPERATURE"
 else
-    ICON=""
+  sketchybar --set "$NAME" drawing=off
 fi
 
-sketchybar --set "$NAME" icon="$ICON" label="$TEMPERATURE"
