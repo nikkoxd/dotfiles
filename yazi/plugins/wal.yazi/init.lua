1d95f2e2410e3a1418e2b9eaabc4cf79445302cb
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-14 23:35:08 +0400

    fix: remove notifications

--- yazi/plugins/wal.yazi/init.lua
+++ yazi/plugins/wal.yazi/init.lua
@@ -7,7 +7,6 @@ end)
 return {
   entry = function()
     local url = get_url()
-    ya.notify { title = "wal", content = url, level = "info", timeout = 5 }
     ya.manager_emit("shell", {
 			"zsh" .. " -ic " .. ya.quote("fwal " .. url .. "; exit", true),
 			block = true,
