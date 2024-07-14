131ea9b5b59ab2ce0a8844b4ffe2b562a5ba90da
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-15 02:24:04 +0400

    fix: rename Russian layout to RU

--- sketchybar/plugins/layout.sh
+++ sketchybar/plugins/layout.sh
@@ -2,5 +2,9 @@
 
 LAYOUT=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'KeyboardLayout Name' | sed 's/.* = \(.*\);/\1/')
 
+case "$LAYOUT" in
+  "Russian") LAYOUT="RU"
+esac
+
 sketchybar --set "$NAME" label="$LAYOUT"
 
