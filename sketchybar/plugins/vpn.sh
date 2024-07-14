630ad709377fc56f1474b5703fff792e8c173245
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-15 02:24:38 +0400

    fix: fix the delay when clicking on item

--- sketchybar/plugins/vpn.sh
+++ sketchybar/plugins/vpn.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-VPN=$(scutil --nc list | grep Connected | sed -E 's/.*"(.*)".*/\1/')
+update() {
+  VPN=$(scutil --nc list | grep Connected | sed -E 's/.*"(.*)".*/\1/')
 
-if [[ $VPN != "" ]]; then
-  sketchybar --set "$NAME" label=$VPN \
-                           click_script='scutil --nc stop "Aeza"'
-else
-  sketchybar --set "$NAME" label="Off" \
-                           click_script='scutil --nc start "Aeza"'
-fi
+  if [[ $VPN != "" ]]; then
+    sketchybar --set "$NAME" label=$VPN
+  else
+    sketchybar --set "$NAME" label="Off"
+  fi
+}
+
+mouse_clicked() {
+  VPN=$(scutil --nc list | grep Connected | sed -E 's/.*"(.*)".*/\1/')
+
+  if [[ $VPN != "" ]]; then
+    scutil --nc stop "Aeza"
+    sketchybar --set "$NAME" label="Off"
+  else
+    scutil --nc start "Aeza"
+    sketchybar --set "$NAME" label="Aeza"
+  fi
+}
+
+case "$SENDER" in
+  "mouse.clicked") mouse_clicked
+  ;;
+  *) update
+  ;;
+esac
