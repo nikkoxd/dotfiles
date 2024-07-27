622ad5b67c84934cf08507649b5f974b7342c861
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-28 01:22:23 +0400

    feat: change accent color to match other clis

--- wal/templates/colors-sketchybar.sh
+++ wal/templates/colors-sketchybar.sh
@@ -2,7 +2,7 @@ export BACKGROUND="0xE6{background.strip}"
 export BACKGROUND2="0xE6{color8.strip}"
 
 export FOREGROUND="0xFF{foreground.strip}"
-export ACCENT="0xFF{color1.strip}"
+export ACCENT="0xFF{color4.strip}"
 
 export WHITE="0xFFFFFFFF"
 export BLACK="0xFF{color0.strip}"
