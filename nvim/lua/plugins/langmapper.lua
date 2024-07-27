462df1642c48b018602477348d8f2d9c290f0300
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-28 01:22:00 +0400

    fix: change how langmapper pulls keys

--- nvim/lua/plugins/langmapper.lua
+++ nvim/lua/plugins/langmapper.lua
@@ -1,23 +1,9 @@
 return {
   "Wansmer/langmapper.nvim",
   priority = 1,
-  opts = {},
-  config = function(_, opts)
-    local function escape(str)
-      local escape_chars = [[;,."|\]]
-      return vim.fn.escape(str, escape_chars)
-    end
-
-    local en = [[`qwertyuiop[]asdfghjkl;'zxcvbnm]]
-    local ru = [[ёйцукенгшщзхъфывапролджэячсмить]]
-    local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>]]
-    local ru_shift = [[ËЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]]
-
-    vim.opt.langmap = vim.fn.join({
-      escape(ru_shift) .. ';' .. escape(en_shift),
-      escape(ru) .. ';' .. escape(en),
-    }, ',')
-
-    require("langmapper").setup(opts)
+  config = function()
+    local langmapper = require("langmapper")
+    langmapper.setup()
+    langmapper.hack_get_keymap()
   end,
 }
