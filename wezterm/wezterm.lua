08a3ed7c567f1803a1209c41ef9cba04d37bcb8b
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-27 23:57:31 +0400

    fix: prettiy wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,13 +5,13 @@ local wezterm = require("wezterm")
 local home = wezterm.home_dir
 local config = wezterm.config_builder()
 
-wezterm.add_to_config_reload_watch_list(home.."/.cache/wal/colors-wezterm.toml")
+wezterm.add_to_config_reload_watch_list(home .. "/.cache/wal/colors-wezterm.toml")
 
 config.font = wezterm.font("FantasqueSansM Nerd Font")
 config.font_size = 18
 config.line_height = 1.1
 
-config.color_scheme_dirs = { home.."/.cache/wal" }
+config.color_scheme_dirs = { home .. "/.cache/wal" }
 config.color_scheme = "colors-wezterm"
 
 config.enable_tab_bar = false
