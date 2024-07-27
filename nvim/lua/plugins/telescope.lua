34c0c465042b330a1feb236229464909f00b39ef
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-28 02:56:07 +0400

    fix: don't lazy load telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.6",
+  lazy = false,
 
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -9,9 +10,9 @@ return {
 
   keys = {
     { "<leader>pf", function() require("telescope.builtin").find_files() end, desc = "Find files" },
-    { "<C-p>", function() require("telescope.builtin").git_files() end, desc = "Git files" },
-    { "<leader>ps", function() require("telescope.builtin").live_grep() end, desc = "Grep string" },
-    { "<leader>vh", function() require("telescope.builtin").help_tags() end, desc = "Help tags" },
+    { "<C-p>",      function() require("telescope.builtin").git_files() end,  desc = "Git files" },
+    { "<leader>ps", function() require("telescope.builtin").live_grep() end,  desc = "Grep string" },
+    { "<leader>vh", function() require("telescope.builtin").help_tags() end,  desc = "Help tags" },
   },
 
   opts = {
