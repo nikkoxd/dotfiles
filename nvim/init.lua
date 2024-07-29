c9be3a287e40be9ddeddc7f8d105728bec75dd12
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-30 03:51:55 +0400

    fix: move the configs to their own folder

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,4 @@
-require("remap")
-require("autocmds")
-require("opts")
-
-local lazypath = vim.fn.stdpath("data") .. "lazy/lazy.nvim"
-
--- load lazy.nvim
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- start godot server if project.godot exists
-local gdproject = io.open(vim.fn.getcwd().."/project.godot", "r")
-if gdproject then
-    io.close(gdproject)
-    vim.fn.serverstart "/tmp/godot.pipe"
-end
-
-require("lazy").setup("plugins")
+require("config.remap")
+require("config.autocmds")
+require("config.opts")
+require("config.lazy")
