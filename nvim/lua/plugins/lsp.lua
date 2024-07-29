2d338be15bc6b5a3fd2e67b8969d730a4b2c8219
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-30 00:47:40 +0400

    fix: add descriptions for lsp keymaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,19 +13,33 @@ return {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(ev)
         local opts = { buffer = ev.buf, silent = true }
+        opts.desc = "LSP Info"
         vim.keymap.set("n", "<leader>cl", "<cmd>LspInfo<cr>", opts)
+        opts.desc = "Show Definitions"
         vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, opts)
+        opts.desc = "Show References"
         vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
+        opts.desc = "Show Implementations"
         vim.keymap.set("n", "gI", require("telescope.builtin").lsp_implementations, opts)
+        opts.desc = "Show Type Definitions"
         vim.keymap.set("n", "gy", require("telescope.builtin").lsp_type_definitions, opts)
+        opts.desc = "Jump to declaration"
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        opts.desc = "Display hover information"
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        opts.desc = "Display signature inforamtion"
         vim.keymap.set("n", "<leader>gk", vim.lsp.buf.signature_help, opts)
+        opts.desc = "Show code actions"
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        opts.desc = "Run code lens"
         vim.keymap.set("n", "<leader>cc", vim.lsp.codelens.run, opts)
+        opts.desc = "Refresh code lens"
         vim.keymap.set("n", "<leader>cC", vim.lsp.codelens.refresh, opts)
+        opts.desc = "Rename"
         vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+        opts.desc = "Show diagnostics"
         vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, opts)
+        opts.desc = "Format current file"
         vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, opts)
       end,
     })
