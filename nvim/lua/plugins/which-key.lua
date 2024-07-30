abfccac9244e91a63008cfa70a7bf26baae83bb7
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-30 23:41:03 +0400

    fix: replace mini with which-key

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>?", function() require("which-key").show({ global = false }) end, desc = "Buffer Local Keymaps (which-key)" },
+  },
+  opts = function()
+    local pywal16_core = require("pywal16.core")
+    local colors = pywal16_core.get_colors()
+
+    vim.api.nvim_set_hl(0, "WhichKeyNormal", { fg = colors.foreground, bg = colors.transparent })
+    vim.api.nvim_set_hl(0, "WhichKeySeparator", { fg = colors.foreground, bg = colors.transparent })
+  end,
+}
