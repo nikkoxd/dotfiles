66c075163b2b07d8bb028d8381bc8e17164a69c6
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-31 05:23:17 +0400

    fix: disable audio bell in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,6 +7,8 @@ local config = wezterm.config_builder()
 
 wezterm.add_to_config_reload_watch_list(home .. "/.cache/wal/colors-wezterm.toml")
 
+config.audible_bell = "Disabled"
+
 config.font = wezterm.font("FantasqueSansM Nerd Font")
 config.font_size = 18
 config.line_height = 1.1
