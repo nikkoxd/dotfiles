b92223ea2e2c033ea178fda551539750644d2555
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-03 18:16:50 +0400

    fix: set homebrew auto update timeout

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,6 +1,13 @@
 export PATH=$PATH:~/.spoof-dpi/bin:~/pickup/target/release:~/Library/Python/3.10/lib/python/site-packages
-export ZSH="$HOME/.oh-my-zsh"
+
+# homebrew
+export HOMEBREW_AUTO_UPDATE_SECS=10224480
+
+# set default editor
 export EDITOR="nvim"
+
+# bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+
 export BAT_THEME="base16"
