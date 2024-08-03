de5858bbb24e7e98198ea15a92e5ffb87c2e0cb9
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-04 02:54:41 +0400

    fix: use dirname instead of pwd

--- install.sh
+++ install.sh
@@ -20,10 +20,10 @@ fi
 # Symlink configs to ~/.config
 if [ -d $HOME/.config ]; then
   echo "Symlinking configs to $HOME/.config"
-  ln -s $(pwd)/*/ $HOME/.config
+  ln -s $(dirname "$0")/*/ $HOME/.config
 else
   echo "\$HOME/.config not found. Creating..."
   mkdir $HOME/.config
   echo "Symlinking configs to $HOME/.config"
-  ln -s $(pwd)/*/ $HOME/.config
+  ln -s $(dirname "$0")/*/ $HOME/.config
 fi
