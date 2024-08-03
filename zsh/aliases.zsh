7df8030a8426c3e2033435c8f95c63fd025d38f5
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-04 01:27:49 +0400

    fix: add more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,11 +3,16 @@ alias src="source ~/.zshrc"
 alias cd="z"
 alias ~="cd ~"
 alias ..="cd ../"
+alias ...="cd ../.."
+
+alias repos="cd repos"
+alias dots="cd dotfiles"
 
 alias cat="bat"
 
 alias ls="eza"
-alias ll="eza -l"
+alias ll="eza -la"
+alias ld="eza -d */"
 
 alias lg="lazygit"
 
