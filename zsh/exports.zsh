71f030cf670de782ab1a2d1556970bd9a57240f3
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-06 02:42:10 +0400

    fix: remove right prompt indent

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,13 +1,7 @@
 export PATH=$PATH:~/.spoof-dpi/bin:~/pickup/target/release:~/Library/Python/3.10/lib/python/site-packages
-
-# homebrew
+export ZLE_RPROMPT_INDENT=0
 export HOMEBREW_AUTO_UPDATE_SECS=10224480
-
-# set default editor
 export EDITOR="nvim"
-
-# bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
-
 export BAT_THEME="base16"
