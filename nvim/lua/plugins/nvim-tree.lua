7c45267ebe8ede924c6f78f57934f16c19a6e1e0
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-11 00:38:42 +0400

    fix: edit nvim-tree config

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -12,6 +12,7 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
+    -- autoclose tree when it's the last buffer
     vim.api.nvim_create_autocmd("QuitPre", {
       callback = function()
         local tree_wins = {}
@@ -37,6 +38,7 @@ return {
 
     return {
       on_attach = function(bufnr)
+        -- preview
         local api = require("nvim-tree.api")
 
         api.config.mappings.default_on_attach(bufnr)
@@ -60,6 +62,42 @@ return {
             end
           end
         end, opts("Preview"))
+
+        -- h, j, k, l style navigation and editing
+        local function edit_or_open()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file
+            api.node.open.edit()
+            -- Close the tree if file was opened
+            api.tree.close()
+          end
+        end
+
+        -- open as vsplit on current node
+        local function vsplit_preview()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file as vsplit
+            api.node.open.vertical()
+          end
+
+          -- Finally refocus on tree if it was lost
+          api.tree.focus()
+        end
+
+        vim.keymap.set("n", "l", edit_or_open,          opts("Edit Or Open"))
+        vim.keymap.set("n", "L", vsplit_preview,        opts("Vsplit Preview"))
+        vim.keymap.set("n", "h", api.tree.close,        opts("Close"))
+        vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
       end,
       view = {
         relativenumber = true,
@@ -80,6 +118,9 @@ return {
       },
       filters = {
         dotfiles = true,
+      },
+      live_filter = {
+        always_show_folders = false,
       },
     }
   end,
