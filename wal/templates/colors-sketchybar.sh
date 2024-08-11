f3d1aa09aeb0592792c06e23abbb632faecd1a37
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-11 16:34:09 +0400

    fix: change transparency of sketchybar items

--- wal/templates/colors-sketchybar.sh
+++ wal/templates/colors-sketchybar.sh
@@ -1,5 +1,5 @@
-export BACKGROUND="0xE6{background.strip}"
-export BACKGROUND2="0xE6{color8.strip}"
+export BACKGROUND="0xFF{background.strip}"
+export BACKGROUND2="0xFF{color8.strip}"
 
 export FOREGROUND="0xFF{foreground.strip}"
 export ACCENT="0xFF{color4.strip}"
