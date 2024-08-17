ad79a9486c0a6c4ceb8cd52c9fd5a1d52dd8a151
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-17 05:34:06 +0400

    fix: add noice

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    cmdline = {
+      view = "cmdline",
+    },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    presets = {
+      lsp_doc_border = true,
+    },
+  },
+}
