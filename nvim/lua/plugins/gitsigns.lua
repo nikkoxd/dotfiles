9acbbe3940b0eb706ee01cefc428376713562319
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-17 06:23:57 +0400

    fix: add keymaps for previewing hunks and git blame

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,10 @@
 return {
   "lewis6991/gitsigns.nvim",
+  lazy = false,
+  keys = {
+    { "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", desc = "Preview hunk" },
+    { "<leader>gt", "<cmd>Gitsigns toggle_current_line_blame<CR>", desc = "Toggle Git blame" },
+  },
   opts = {
     signs = {
       add = { text = "+" },
