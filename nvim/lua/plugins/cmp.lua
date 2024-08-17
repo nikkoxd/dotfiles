f5b88af9775a64dbac8c729dc5886b0bd2499f3c
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-17 05:49:36 +0400

    fix: add cmdline completion

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -7,6 +7,7 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-emoji",
+    "hrsh7th/cmp-cmdline",
     {
       "L3MON4D3/LuaSnip",
       version = "v2.*",
@@ -23,6 +24,27 @@ return {
     local lspkind = require("lspkind")
 
     require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        {
+          name = "cmdline",
+          option = {
+            ignore_cmds = { "Man", "!" },
+          }
+        }
+      })
+    })
 
     return {
       completion = {
