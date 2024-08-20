fca8cb8ba6bc0322e5158700f0ae56c613e1c623
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-20 05:39:36 +0400

    fix: re-enable messages

--- nvim/lua/plugins/ui/noice.lua
+++ nvim/lua/plugins/ui/noice.lua
@@ -9,9 +9,6 @@ return {
     cmdline = {
       view = "cmdline",
     },
-    messages = {
-      enabled = false
-    },
     lsp = {
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
