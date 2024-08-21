089f3f07b82af4f9a819bc20529acc7cd76d16c3
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-21 04:47:05 +0400

    fix: update spoof-dpi icon on sketchybar load

--- sketchybar/items/spoofdpi.sh
+++ sketchybar/items/spoofdpi.sh
@@ -1,6 +1,4 @@
 spoofdpi=(
-  icon=􀎠
-  icon.color=$DISABLED
   icon.padding_left=10
   icon.padding_right=10
   label.drawing=off
@@ -9,3 +7,9 @@ spoofdpi=(
 
 sketchybar --add item spoofdpi right \
            --set spoofdpi "${spoofdpi[@]}"
+
+if [[ $(launchctl list | grep -i spoof-dpi) ]]; then
+  sketchybar --set spoofdpi icon=􀎤 icon.color=$ACCENT
+else
+  sketchybar --set spoofdpi icon=􀎠 icon.color=$DISABLED
+fi
