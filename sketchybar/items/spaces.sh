698caebec8ae9815c1652327d723dcc3e4f94915
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-25 01:12:20 +0400

    fix: refactor spaces.sh

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -3,7 +3,7 @@
 SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
 for i in "${!SPACE_ICONS[@]}"
 do
-  sid="$(($i+1))"
+  sid="$((i+1))"
   space=(
     space="$sid"
     icon="${SPACE_ICONS[i]}"
