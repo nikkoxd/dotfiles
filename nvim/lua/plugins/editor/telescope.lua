b4230a7ae2edac111923b9d625c901765372470a
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-28 03:13:26 +0400

    fix: add keymap for viewing hidden files thru telescope

--- nvim/lua/plugins/editor/telescope.lua
+++ nvim/lua/plugins/editor/telescope.lua
@@ -10,6 +10,7 @@ return {
 
   keys = {
     { "<leader>pf", function() require("telescope.builtin").find_files() end, desc = "Find files" },
+    { "<leader>ph", function() require("telescope.builtin").find_files({ hidden = true, no_ignore = true }) end, desc = "Find files (hidden, no_ignore)" },
     { "<C-p>",      function() require("telescope.builtin").git_files() end,  desc = "Git files" },
     { "<leader>ps", function() require("telescope.builtin").live_grep() end,  desc = "Grep string" },
     { "<leader>vh", function() require("telescope.builtin").help_tags() end,  desc = "Help tags" },
