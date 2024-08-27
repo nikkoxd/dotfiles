0a78f92aef5ae6bde879af5fc80f984bde04858e
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-08-27 22:28:15 +0400

    fix: display only non-empty workspaces

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 update() {
   if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
@@ -6,6 +6,14 @@ update() {
   else
     sketchybar --set "$NAME" background.drawing=off
   fi
+
+  for sid in $(aerospace list-workspaces --monitor all --empty); do
+    sketchybar --set "space.$sid" label.drawing=off
+  done
+
+  for sid in $(aerospace list-workspaces --monitor focused --empty no); do
+    sketchybar --set "space.$sid" label.drawing=on
+  done
 }
 
 mouse_clicked() {
