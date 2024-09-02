39438d070f07d6072d05627c7b35895cce2b98a9
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-09-02 15:09:31 +0400

    fix: change mic icons

--- sketchybar/plugins/mic.sh
+++ sketchybar/plugins/mic.sh
@@ -6,8 +6,8 @@ MIC_VOLUME=$(osascript -e 'input volume of (get volume settings)')
 
 if [[ $MIC_VOLUME -eq 0 ]]; then
   osascript -e 'set volume input volume 100'
-  sketchybar --set "$NAME" icon="􀊳" icon.color="$DISABLED"
+  sketchybar --set "$NAME" icon="􀊱" icon.color="$ACCENT"
 elif [[ $MIC_VOLUME -gt 0 ]]; then
   osascript -e 'set volume input volume 0'
-  sketchybar --set "$NAME" icon="􀊱" icon.color="$ACCENT"
+  sketchybar --set "$NAME" icon="􀊳" icon.color="$DISABLED"
 fi
