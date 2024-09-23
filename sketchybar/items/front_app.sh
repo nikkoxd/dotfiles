27d500457c6df82550376abde9dbf0e7ae5f0344
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-09-24 00:27:00 +0400

    fix: add paddings to front_app and battery

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -1,3 +1,3 @@
 sketchybar --add item front_app left \
-           --set front_app script="$PLUGIN_DIR/front_app.sh" \
+           --set front_app icon.padding_left=10 label.padding_right=0 script="$PLUGIN_DIR/front_app.sh" \
            --subscribe front_app front_app_switched
