df3dda96313a9330b39027834d4651c02e30cb96
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-09-24 00:17:41 +0400

    fix: edit how highlighted space looks

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -10,13 +10,13 @@ do
     padding_left=0
     padding_right=0
     icon.font="$FONT:Regular:14.0"
-    icon.padding_left=10
-    icon.padding_right=10
+    icon.padding_left=11
+    icon.padding_right=13
     icon.background.color="$TRANSPARENT"
     icon.color="$FOREGROUND"
-    icon.highlight_color="$ACCENT"
-    background.color="$BACKGROUND2"
-    background.corner_radius=0
+    icon.highlight_color="$BACKGROUND"
+    background.color="$ACCENT"
+    background.corner_radius=100
     label.drawing=off
     script="$PLUGIN_DIR/space.sh"
   )
