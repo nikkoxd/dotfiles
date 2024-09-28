a6346efe30ae2a91f1474efb540fc2099b99efb4
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-09-28 20:41:14 +0400

    fix: change spicetify theme to text

--- spicetify/Themes/Comfy/update-colors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp -r "$HOME/.cache/wal/colors-spicetify.ini" "$HOME/.config/spicetify/Themes/Comfy"
-cd "$HOME/.config/spicetify/Themes/Comfy/" || return
-mv colors-spicetify.ini color.ini
