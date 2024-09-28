77f0cdce288eb151418aea581c23275ded9801a9
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-09-28 20:30:19 +0400

    fix: highlight icon when app menus are shown

--- sketchybar/plugins/flower.sh
+++ sketchybar/plugins/flower.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source "$HOME/.config/sketchybar/colors.sh"
+
 ITEMS_TO_HIDE=("front_app" "weather" "mic")
 DRAWING=$(sketchybar --query "${ITEMS_TO_HIDE[0]}" | jq -r '.geometry.drawing')
 
@@ -7,8 +9,10 @@ if [ "$DRAWING" == "on" ]; then
   for i in "${!ITEMS_TO_HIDE[@]}"; do
     sketchybar --set "${ITEMS_TO_HIDE[i]}" drawing=off
   done
+  sketchybar --set "$NAME" icon.color="$BACKGROUND" background.color="$ACCENT"
   sketchybar --set "/menu\..*/" drawing=on
 else
+  sketchybar --set "$NAME" icon.color="$ACCENT" background.color="$TRANSPARENT"
   sketchybar --set "/menu\..*/" drawing=off
   for i in "${!ITEMS_TO_HIDE[@]}"; do
     sketchybar --set "${ITEMS_TO_HIDE[i]}" drawing=on
