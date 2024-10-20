491e9d0a269394eafcd2372b384e632821eb635d
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-10-20 15:26:37 +0400

    fix: change highlight color for mic.sh

--- sketchybar/plugins/mic.sh
+++ sketchybar/plugins/mic.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-source "$HOME/.config/sketchybar/colors.sh"
+source "$HOME/.config/sketchybar/colors-my.sh"
 
 MIC_VOLUME=$(osascript -e 'input volume of (get volume settings)')
 
 if [[ $MIC_VOLUME -eq 0 ]]; then
   osascript -e 'set volume input volume 100'
-  sketchybar --set "$NAME" icon="􀊱" icon.color="$ACCENT"
+  sketchybar --set "$NAME" icon="􀊱" icon.color="$primary" background.color="$containerBackground"
 elif [[ $MIC_VOLUME -gt 0 ]]; then
   osascript -e 'set volume input volume 0'
-  sketchybar --set "$NAME" icon="􀊳" icon.color="$DISABLED"
+  sketchybar --set "$NAME" icon="􀊳" icon.color="$background" background.color="$primary"
 fi
