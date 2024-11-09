da44c5be1388156c81429ee37060a729cca8b919
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-11-10 02:48:15 +0400

    fix: add prettier

--- .config/nvim/lua/plugins/lsp/lsp.lua
+++ .config/nvim/lua/plugins/lsp/lsp.lua
@@ -53,13 +53,42 @@ return {
     }
 
     local navic = require("nvim-navic")
+    local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+    local event = "BufWritePre" -- or "BufWritePost"
+    local async = event == "BufWritePost"
     local on_attach = function(client, bufnr)
       if client.server_capabilities.documentSymbolProvider then
         navic.attach(client, bufnr)
       end
+
+      if client.supports_method("textDocument/formatting") then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    if client.supports_method("textDocument/rangeFormatting") then
+      vim.keymap.set("x", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
     end
 
     local lspconfig = require("lspconfig")
+
+    lspconfig.nushell.setup{}
+
     require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = {
