210197bd93d3d3466c1f397d05e9fe4f46a10806
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-11-22 02:28:40 +0400

    fix: move supermaven to ai folder

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
+    { import = "plugins.ai" },
     { import = "plugins.colorschemes" },
     { import = "plugins.editor" },
     { import = "plugins.git" },
