afa6fdf2bcfd25c54e325cb385accb731f1d1f69
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-11-28 21:58:44 +0400

    fix: update lualine theme colors

--- .config/nvim/lua/lualine/themes/custom_neopywal.lua
+++ .config/nvim/lua/lualine/themes/custom_neopywal.lua
@@ -1,5 +1,6 @@
 local M = require("neopywal.theme.plugins.lualine").get()
 local C = require("neopywal.lib.palette").get_colors()
+local U = require("neopywal.utils.color")
 
 M.normal = {
   a = { bg = C.color4, fg = C.background, gui = "bold" },
@@ -8,8 +9,8 @@ M.normal = {
   b = { bg = C.color0 },
   y = { bg = C.color0 },
 
-  c = { bg = C.none },
-  x = { bg = C.none },
+  c = { bg = U.darken(C.background, 5) },
+  x = { bg = U.darken(C.background, 5) },
 }
 
 return M
