d36e9741a8ac27df10e1a7a3eae2cf2ef2c55c20
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-12-14 04:08:57 +0400

    fix: symlink wezterm colorscheme to wezter/colorschemes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,26 +5,30 @@ local wezterm = require("wezterm")
 local home = wezterm.home_dir
 local config = wezterm.config_builder()
 
-wezterm.add_to_config_reload_watch_list(home .. "/.cache/wal/colors-wezterm.toml")
+wezterm.add_to_config_reload_watch_list(home .. "/.dotfiles/.config/wezterm/colorschemes/colors-wezterm.toml")
 
 config.audible_bell = "Disabled"
 
 config.font = wezterm.font_with_fallback({
-  { family = "JetBrainsMono Nerd Font", weight = "Medium" },
+  "BerkeleyMono Nerd Font",
+  "JetBrains Mono",
   "Apple Color Emoji",
-  "Symbols Nerd Font",
   "SF Pro"
 })
 config.font_size = 14
 config.line_height = 1.1
 
-config.color_scheme_dirs = { home .. "/.cache/wal" }
+config.color_scheme_dirs = { home .. "/.dotfiles/.config/wezterm/colorschemes" }
 config.color_scheme = "colors-wezterm"
 
 config.enable_tab_bar = false
 
 config.window_background_opacity = 1
-config.macos_window_background_blur = 10
+config.macos_window_background_blur = 50
+
+config.window_padding = {
+  top = 20,
+}
 
 config.window_decorations = "RESIZE"
 
