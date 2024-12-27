33dd8b0a3ba89e27b338b972e54c52042abc84fe
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-12-27 17:29:18 +0400

    fix: map accept to <CR>, enable manual selection

--- .config/nvim/lua/plugins/lsp/blink.lua
+++ .config/nvim/lua/plugins/lsp/blink.lua
@@ -6,11 +6,27 @@ return {
   version = "*",
 
   opts = {
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "default",
+
+      ["<CR>"] = { "accept" },
+
+      cmdline = {
+        preset = "enter",
+
+        ["<CR>"] = { "accept", "fallback" },
+      }
+    },
 
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono"
+    },
+
+    completion = {
+      list = {
+        selection = "manual",
+      }
     },
 
     sources = {
