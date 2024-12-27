e3aceb2754009ab3c074f484a8169b44e4d522cc
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-12-27 14:22:45 +0400

    fix: edit lualine theme

--- .config/nvim/lua/lualine/themes/custom_neopywal.lua
+++ .config/nvim/lua/lualine/themes/custom_neopywal.lua
@@ -2,8 +2,8 @@ local M = require("neopywal.theme.plugins.lualine").get()
 local C = require("neopywal.lib.palette").get_colors()
 
 M.normal = {
-  a = { bg = C.none, fg = C.color4, gui = "bold" },
-  z = { bg = C.none, fg = C.color4, gui = "bold" },
+  a = { bg = C.none, fg = C.color4 },
+  z = { bg = C.none, fg = C.color4 },
 
   b = { bg = C.none },
   y = { bg = C.none },
@@ -13,28 +13,28 @@ M.normal = {
 }
 
 M.visual = {
-  a = { bg = C.none, fg = C.color5, gui = "bold" },
-  z = { bg = C.none, fg = C.color5, gui = "bold" },
+  a = { bg = C.none, fg = C.color5 },
+  z = { bg = C.none, fg = C.color5 },
 }
 
 M.insert = {
-  a = { bg = C.none, fg = C.color6, gui = "bold" },
-  z = { bg = C.none, fg = C.color6, gui = "bold" },
+  a = { bg = C.none, fg = C.color6 },
+  z = { bg = C.none, fg = C.color6 },
 }
 
 M.command = {
-  a = { bg = C.none, fg = C.color1, gui = "bold" },
-  z = { bg = C.none, fg = C.color1, gui = "bold" },
+  a = { bg = C.none, fg = C.color1 },
+  z = { bg = C.none, fg = C.color1 },
 }
 
 M.replace = {
-  a = { bg = C.none, fg = C.color2, gui = "bold" },
-  z = { bg = C.none, fg = C.color2, gui = "bold" },
+  a = { bg = C.none, fg = C.color2 },
+  z = { bg = C.none, fg = C.color2 },
 }
 
 M.terminal = {
-  a = { bg = C.none, fg = C.color3, gui = "bold" },
-  z = { bg = C.none, fg = C.color3, gui = "bold" },
+  a = { bg = C.none, fg = C.color3 },
+  z = { bg = C.none, fg = C.color3 },
 }
 
 return M
