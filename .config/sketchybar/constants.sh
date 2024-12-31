43e11eb2d02b6280f9ce16a5e615d029ebb11ff1
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-12-31 16:56:26 +0400

    fix: move constants from rc to constants.sh

--- .config/sketchybar/constants.sh
+++ .config/sketchybar/constants.sh
@@ -1,2 +1,8 @@
-export VPN_NAME="FoXray"
+export ITEM_DIR="$CONFIG_DIR/items"
+export PLUGIN_DIR="$CONFIG_DIR/plugins"
+export HELPERS_DIR="$CONFIG_DIR/helpers"
 
+export FONT="Google Sans"
+export ICON_FONT="SF Pro"
+
+export VPN_NAME="FoXray"
