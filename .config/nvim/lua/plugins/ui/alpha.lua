d0eb1dc1d20808817fd9e8bbc22211fdf92a451f
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-01-07 06:56:59 +0400

    fix: update alpha config

--- .config/nvim/lua/plugins/ui/alpha.lua
+++ .config/nvim/lua/plugins/ui/alpha.lua
@@ -6,29 +6,20 @@ return {
     local dashboard = require("alpha.themes.dashboard")
 
     local function footer()
-      local total_plugins = require("lazy").stats().count
       local version = vim.version()
-      local nvim_version_info = "     " .. version.major .. "." .. version.minor .. "." .. version.patch
-
-      return "󰚥  " .. total_plugins .. " plugins " .. nvim_version_info
+      return "ver " .. version.major .. "." .. version.minor .. "." .. version.patch
     end
 
-    dashboard.section.header.val = {
-      " ▐ ▄ ▄▄▄ .       ▌ ▐·▪  • ▌ ▄ ·. ",
-      "•█▌▐█▀▄.▀· ▄█▀▄ ▪█·█▌██ ·██ ▐███▪",
-      "▐█▐▐▌▐▀▀▪▄▐█▌.▐▌▐█▐█•▐█·▐█ ▌▐▌▐█·",
-      "██▐█▌▐█▄▄▌▐█▌.▐▌ ███ ▐█▌██ ██▌▐█▌",
-      "▀▀ █▪ ▀▀▀  ▀█▄▀▪. ▀  ▀▀▀▀▀  █▪▀▀▀",
-    }
+    dashboard.section.header.val = "neovim"
 
     dashboard.section.buttons.val = {
-      dashboard.button("e", "󰈔  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("SPC p f", "󰈞  Find file"),
-      dashboard.button("SPC p r", "󱋡  Recent"),
-      dashboard.button("SPC p v", "󰉋  Tree view"),
-      dashboard.button("SPC q s", "󰦛  Restore session"),
-      dashboard.button("l", "󰒲  View plugins", ":Lazy<CR>"),
-      dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+      dashboard.button("e", "new file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("spc p f", "find file"),
+      dashboard.button("spc p r", "recent"),
+      dashboard.button("spc p v", "tree view"),
+      dashboard.button("spc q s", "restore session"),
+      dashboard.button("l", "view plugins", ":Lazy<CR>"),
+      dashboard.button("q", "quit neovim", ":qa<CR>"),
     }
 
     dashboard.section.footer.val = footer()
