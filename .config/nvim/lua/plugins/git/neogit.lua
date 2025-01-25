847506a4bdcc49e3bfa5165e0db077911c93ae46
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-01-25 21:21:36 +0400

    fix: replace lazygit with neogit

---
+++ .config/nvim/lua/plugins/git/neogit.lua
@@ -0,0 +1,13 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+  },
+  opts = {}
+}
+
