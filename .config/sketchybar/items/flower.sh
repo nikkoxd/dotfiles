058f64d69e2748a55ed3f1e85de21ed44a8d1862
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-03-21 19:23:21 +0400

    chore: remove unused scripts

--- .config/sketchybar/items/flower.sh
+++
@@ -1,8 +0,0 @@
-flower=(
-  icon="􀇁"
-  icon.padding_right=12
-  label.drawing=off
-  click_script="$PLUGIN_DIR/flower.sh"
-)
-sketchybar --add item flower left \
-           --set flower "${flower[@]}"
--- .config/sketchybar/items/wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item wifi right                  \
-           --set wifi script="$PLUGIN_DIR/wifi.sh" \
-                     updates=on                  \
-                     icon.padding_right=10       \
-                     label.drawing=off           \
-           --subscribe wifi wifi_change
