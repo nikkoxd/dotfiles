b73269c8853a78d91a1838a84534d61ba3d1ae4a
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-03-21 20:52:59 +0400

    fix: fix mask not applying to cover image

--- .config/sketchybar/plugins/spotify.sh
+++ .config/sketchybar/plugins/spotify.sh
@@ -22,8 +22,8 @@ set_cover() {
   COVER=$(osascript -e 'tell application "Spotify" to get artwork url of current track')
 
   curl -s --max-time 20 "$COVER" -o /tmp/cover.png
-  magick -size 640x640 xc:none -draw "roundrectangle 0,0,640,640,100,100" /tmp/mask.png
-  magick /tmp/cover.png -gravity Center -resize 640x640 -alpha set /tmp/cover.png -compose DstIn -composite /tmp/cover.png
+  magick -size 640x640 xc:none -draw "roundrectangle 0,0,640,640,50,50" /tmp/mask.png
+  magick /tmp/cover.png -gravity Center -extent 1:1 -resize 640x640 -alpha set /tmp/mask.png -compose DstIn -composite /tmp/cover.png
 
   sketchybar --set spotify.cover background.image="/tmp/cover.png"
 }
