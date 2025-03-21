99c75e2bb3f09f99fc21933c016bc5e20e68936c
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-03-21 20:06:50 +0400

    feat: add notifications

--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+front_app=(
+  popup.drawing=on
+  popup.y_offset=-100
+  script="$PLUGIN_DIR/front_app.sh"
+)
+
+notification=(
+  label="Notification"
+  icon="􀋚"
+  script="$PLUGIN_DIR/notification.sh"
+)
+
 sketchybar --add item front_app left \
-           --set front_app script="$PLUGIN_DIR/front_app.sh" \
+           --set front_app "${front_app[@]}" \
            --subscribe front_app front_app_switched
+
+sketchybar --add event notification_triggered \
+           --add item notification popup.front_app \
+           --set notification "${notification[@]}" \
+           --subscribe notification notification_triggered
