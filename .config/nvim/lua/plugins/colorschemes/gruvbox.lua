34daf9f631f767e68a3693b99d1b162fadeb32a7
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-04-16 00:55:31 +0400

    fix: remove unused colorschemes

--- .config/nvim/lua/plugins/colorschemes/gruvbox.lua
+++
@@ -1,8 +0,0 @@
-local vars = require("config.vars")
-
-return {
-  "ellisonleao/gruvbox.nvim",
-  opts = {
-    transparent_mode = vars.transparency,
-  }
-}
--- .config/nvim/lua/plugins/colorschemes/kanagawa.lua
+++
@@ -1,8 +0,0 @@
-local vars = require "config.vars"
-
-return {
-  "rebelot/kanagawa.nvim",
-  opts = {
-    transparent = vars.transparency,
-  }
-}
