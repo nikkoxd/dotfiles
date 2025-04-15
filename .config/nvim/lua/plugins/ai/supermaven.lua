9f48cf18875e83d0e2606c64dedce2c44b0d7b59
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-04-16 00:56:04 +0400

    fix: remove duplicate supermaven config

--- .config/nvim/lua/plugins/ai/supermaven.lua
+++ .config/nvim/lua/plugins/ai/supermaven.lua
@@ -1,9 +1,7 @@
 return {
   "supermaven-inc/supermaven-nvim",
-  config = function()
-    require("supermaven-nvim").setup {
-      disable_keymaps = true,
-      disable_inline_completion = true,
-    }
-  end,
+  opts = {
+    disable_keymaps = true,
+    disable_inline_completion = true,
+  }
 }
--- .config/nvim/lua/plugins/misc/supermaven.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "supermaven-inc/supermaven-nvim",
-  opts = {
-    log_level = "off",
-    disable_inline_completion = false,
-  }
-}
