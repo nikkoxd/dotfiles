a363eb491126867dcacb15397082ca04c84b5bf1
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-04-26 22:30:36 +0400

    fix(sketchybar): change font to manrope

--- .config/sketchybar/constants.sh
+++ .config/sketchybar/constants.sh
@@ -4,7 +4,7 @@ export ITEM_DIR="$CONFIG_DIR/items"
 export PLUGIN_DIR="$CONFIG_DIR/plugins"
 export HELPERS_DIR="$CONFIG_DIR/helpers"
 
-export FONT="Google Sans"
+export FONT="Manrope"
 export ICON_FONT="SF Pro"
 
 export VPN_NAME="FoXray"
