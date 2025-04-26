dea472051627e14c0d8849830694e2073ec56d21
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-04-26 22:30:24 +0400

    fix(sketchybar): remove old colors

--- .config/sketchybar/colors-my.sh
+++
@@ -1,6 +0,0 @@
-source "/Users/nikko/.cache/wal/colors-materialyou.sh"
-
-export background="0xFF${surface}"
-export containerBackground="0xFF${surface_container}"
-export primary="0xFF${primary}"
-export text="0xFF${on_surface}"
