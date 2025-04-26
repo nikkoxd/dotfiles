fe1195553cd227e7a6e730573542de53fe665e6c
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-04-26 22:28:35 +0400

    fix(nvim): add aerial

---
+++ .config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,11 @@
+return {
+  "stevearc/aerial.nvim",
+  dependencies = {
+     "nvim-treesitter/nvim-treesitter",
+     "echasnovski/mini.icons"
+  },
+  keys = {
+    { "<leader>a", "<cmd>AerialToggle!<CR>", desc = "Toggle Aerial" },
+  },
+  opts = {},
+}
