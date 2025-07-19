759e420c29416127fbbb88d58ca95d4d8e5acce5
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-07-19 22:17:18 +0400

    fix(sketchybar): update airpods icons

--- .config/sketchybar/plugins/airpods.sh
+++ .config/sketchybar/plugins/airpods.sh
@@ -1,53 +1,57 @@
 #!/usr/bin/env bash
-  
-show_both_earbuds() {
-  update
 
-  sketchybar --animate sin 15 --set "$NAME" label="Left: $left%, Right: $right%, Case: $case%"
+show_both_earbuds() {
+    update
+
+    sketchybar --animate sin 15 --set "$NAME" label="(􀲎 $left 􀲍 $right 􀹬 $case)"
 }
 
 update() {
-  DEVICES="$(system_profiler SPBluetoothDataType -json -detailLevel basic 2>/dev/null | jq -rc '.SPBluetoothDataType[0].device_connected[] | select ( .[] | .device_minorType == "Headphones")' | jq '.[]')"
-  if [ "$DEVICES" = "" ]; then
-    sketchybar -m --set "$NAME" drawing=off
-  else
-    sketchybar -m --set "$NAME" drawing=on
-    left="$(echo "$DEVICES" | jq -r .device_batteryLevelLeft | tr -d '[:space:]%')"
-    right="$(echo "$DEVICES" | jq -r .device_batteryLevelRight | tr -d '[:space:]%')"
-    case="$(echo "$DEVICES" | jq -r .device_batteryLevelCase | tr -d '[:space:]%')"
-
-    if [[ "$left" -gt "$right" ]]; then
-      percentage="$right"
+    DEVICES="$(system_profiler SPBluetoothDataType -json -detailLevel basic 2>/dev/null | jq -rc '.SPBluetoothDataType[0].device_connected[] | select ( .[] | .device_minorType == "Headphones")' | jq '.[]')"
+    if [ "$DEVICES" = "" ]; then
+        sketchybar -m --set "$NAME" drawing=off
     else
-      percentage="$left"
-    fi
+        sketchybar -m --set "$NAME" drawing=on
+        left="$(echo "$DEVICES" | jq -r .device_batteryLevelLeft | tr -d '[:space:]')"
+        right="$(echo "$DEVICES" | jq -r .device_batteryLevelRight | tr -d '[:space:]')"
+        case="$(echo "$DEVICES" | jq -r .device_batteryLevelCase | tr -d '[:space:]')"
+        icon="􀪷"
 
-    if [[ "$left" == "null"  ]]; then
-      left="Nf"
-      percentage="$right"
-    fi
+        if [[ "$left" -gt "$right" ]]; then
+            percentage="$right"
+        else
+            percentage="$left"
+        fi
 
-    if [[ "$right" == "null" ]]; then
-      right='Nf'
-      percentage="$left"
-    fi
+        if [[ "$left" == "null" ]]; then
+            left="-"
+            percentage="$right"
+            icon="􀲍"
+        fi
 
-    if [[ "$case" == "null" ]]; then
-      case='Nf'
-    fi
+        if [[ "$right" == "null" ]]; then
+            right="-"
+            percentage="$left"
+            icon="􀲎"
+        fi
 
-    sketchybar --animate sin 15 --set "$NAME" label="$percentage%"
-  fi
+        if [[ "$case" == "null" ]]; then
+            case="-"
+        fi
+
+        sketchybar --animate sin 15 --set "$NAME" icon="$icon" label="$percentage" updates=off
+    fi
 }
 
 case "$SENDER" in
-  mouse.entered)
+mouse.entered)
     show_both_earbuds
     ;;
-  mouse.exited)
+mouse.exited)
+    sketchybar --set "$NAME" updates=on
     update
     ;;
-  *)
+*)
     update
     ;;
 esac
