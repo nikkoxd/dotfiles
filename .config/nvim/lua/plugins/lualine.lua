647bedf557e31a84d3e350f6f8ee83c3b328d8a5
nikko <35370166+nikkoxd@users.noreply.github.com>
2026-02-07 23:19:48 +0400

    fix lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,72 +4,51 @@ return {
 		"echasnovski/mini.icons",
 	},
 
-    return {
-      options = {
-        theme = function()
-            local colorscheme = require("config.vars").colorscheme
-            if colorscheme == "neopywal" then
-                return "custom_neopywal"
-            else
-                return colorscheme
-            end
-        end,
-        component_separators = "",
-        section_separators = "",
-        globalstatus = true,
-      },
-      sections = {
-        lualine_a = {
-          {
-            "mode",
-            fmt = function(str)
-              return string.sub(str, 1, 1):upper()
-            end,
-          }
-        },
-        lualine_b = {
-          {
-            "filetype",
-            icon_only = true,
-            padding = {
-              left = 2,
-            },
-          },
-          {
-            "filename",
-            symbols = {
-              modified = "(modified)",
-              readonly = "(readonly)",
-              unnamed = "unnamed",
-              newfile = "new",
-            },
-          },
-          {
-            "lsp_status",
-            icon = "",
-            color = "lualine_lsp",
-            padding = {
-              left = 0,
-            }
-          },
-        },
-        lualine_c = {
-          "diagnostics",
-        },
-        lualine_x = {
-          "overseer",
-          {
-            "branch",
-            icon = { "", color = "lualine_icon_branch" }
-          },
-        },
-        lualine_y = {
-          "progress",
-        },
-        lualine_z = {
-          "location",
-        }
-      }
-    }
-  end,
+	opts = function()
+		return {
+			options = {
+				theme = function()
+					local colorscheme = require("config.vars").colorscheme
+					if colorscheme == "neopywal" then
+						return "custom_neopywal"
+					else
+						return colorscheme
+					end
+				end,
+				component_separators = "",
+				section_separators = "",
+				globalstatus = true,
+			},
+			sections = {
+				lualine_a = {},
+				lualine_b = {
+					{
+						"filename",
+						symbols = {
+							modified = "(modified)",
+							readonly = "(readonly)",
+							unnamed = "unnamed",
+							newfile = "new",
+						},
+						padding = {
+							left = 0,
+							right = 1,
+						},
+					},
+				},
+				lualine_c = {
+					"diagnostics",
+				},
+				lualine_x = {
+					"overseer",
+				},
+				lualine_y = {
+					"location",
+				},
+				lualine_z = {
+					"progress",
+				},
+			},
+		}
+	end,
 }
